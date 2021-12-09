dc68a058c3b70fcb1a33d7a2d0b808c5eb5ba473
d867291 <>
2021-12-09 04:13:32 +0000

    repl.it

--- BOJ_1157.py
+++ BOJ_1157.py
@@ -1,13 +1,26 @@
 usage = dict()
-
 txt = input()
 txt = list(txt.upper())
-print(type(txt))
 
-for idx in str:
+for idx in txt:
   if idx not in usage:
     usage[idx] = 1
   else:
     usage[idx] += 1
 
-print(usage)
+nums = []
+for idx in usage:
+  nums.append(usage[idx])
+
+nums.sort()
+nums.reverse()
+
+if len(nums) == 1:
+  print(txt[0])
+elif nums[0] == nums[1]:
+  print('?')
+else:
+  for idx in usage: 
+    if usage[idx] == nums[0]:
+      print(idx)
+      break
