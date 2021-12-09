3fd80473817a0ef5f9cdfa82701bd33186e4bb3f
d867291 <>
2021-12-09 02:49:36 +0000

    repl.it

--- BOJ_10809.py
+++ BOJ_10809.py
@@ -2,4 +2,8 @@ import sys
 str = sys.stdin.readline().rstrip()
 #print(str)
 
-print(ord(a))
+for i in range(ord('z') - ord('a')+1):
+  #print(chr(ord('a')+i))
+  char = chr(ord('a')+i)
+  answer = str.index(char)
+  print(answer)
